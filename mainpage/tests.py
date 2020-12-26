06fb0ae0b41f057ee2a6e826758ddaf4987be2bc
Denis Skulovic <dskulovich@gmail.com>
2020-12-26 23:59:10 +0200

    added mainpage tests

--- mainpage/tests.py
+++ mainpage/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse, resolve
+from mainpage.views import MainpageTemplateView
 
-# Create your tests here.
+
+
+class MainpageTests(SimpleTestCase):
+    
+    def setUp(self):
+        url = reverse('mainpage')
+        self.response = self.client.get(url)
+
+    def test_mainpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_mainpage_url_name(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_mainpage_template(self):
+        self.assertTemplateUsed(self.response, 'mainpage.html')
+
+    def test_mainpage_contains_correct_html(self):
+        self.assertContains(self.response, 'TEMPORARY MAINPAGE TEXT') ###############
+
+    def test_mainpage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, 'SOME TEXT THAT SHOULD NOT BE THERE') #############
+
+    def test_mainpage_url_resolves_mainpageview(self):
+        view = resolve('/')
+        self.assertEqual(
+            view.func.__name__,
+            MainpageTemplateView.as_view().__name__,
+        )
