10d73312ab2258103fa24fc6a4851fdb4ca1c3a3
Denis Skulovic <dskulovich@gmail.com>
2020-12-27 22:05:17 +0200

    added environment variables to key and debug

--- finance_proj/settings.py
+++ finance_proj/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e7688pwkg74i88%c6i*m0ipuf)(==l07ozf^p^jc195i6-7!cp'
+SECRET_KEY = os.environ.get('SECRET_KEY') #get the secret key from docker-compose.yml
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
